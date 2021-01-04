0e234c197d2dfe46e36404f40a9663565e1f0311
aroraapankaj123 <aroraapankaj123@gmail.com>
2021-01-05 03:23:18 +0530

    added the workspace

---
+++ modules/happyWorld/src/main/java/com/liferay/happyWorld/constants/HappyWorldPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.happyWorld.constants;
+
+/**
+ * @author user
+ */
+public class HappyWorldPortletKeys {
+
+	public static final String HAPPYWORLD =
+		"com_liferay_happyWorld_HappyWorldPortlet";
+
+}
---
+++ modules/happyWorld/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,7 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="happyworld.caption"/></b>
+</p>
+
+<h1>Hello This is Happy world</h1>
